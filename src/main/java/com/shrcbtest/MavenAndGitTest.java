26378282a7f153fcd67d773a82120a34466ddb6a
MartinChan709 <MartinChan709@gmail.com>
2022-08-31 21:26:03 +0800

    由master编写的代码，第一次提交！

---
+++ src/main/java/com/shrcbtest/MavenAndGitTest.java
@@ -0,0 +1,14 @@
+package com.shrcbtest;
+
+/**
+ * @author code clone detection group
+ * @version 1.0
+ * @email martinchan709@gmail.com
+ * @date 2022/8/31 21:12
+ */
+public class MavenAndGitTest {
+    public static void main(String[] args) {
+        System.out.println("由用户master编写");
+    }
+
+}
